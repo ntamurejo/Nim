f39090511a4feb2286c55898ae0e702a42c52620
Noelia Tamurejo Colorado <noelia.tc89@gmail.com>
2018-04-04 20:48:53 +0200

    Added json library

--- samples/var/main.cpp
+++ samples/var/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include "foo.h"
+#include "jsonFile.h"
 
 int main()
 {
     std::cout << "Main..."<< std::endl;
     Foo foo;
     foo.test();
+    
+    jsonFileNamespace::jsonReader fileJSON;
+    fileJSON.readFileJSONFromPath("/Users/ntamurejo/Proyectos/Json/jsonmanual.json");
+
+    
     
     return 0;
 }
